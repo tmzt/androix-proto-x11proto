44e24a27bca023cf7b799f191fe6d52e12efbe5f
James Cloos <cloos@jhcloos.com>
2008-04-12 15:48:46 -0400

    Add more dead key syms Add dead keys to support input of the precomposed latin characters in ISO 10747 which have ring, line, circumflex, tilde, breve or diaeresis below.
    This addresses:  https://bugs.freedesktop.org/show_bug.cgi?id=15446

--- keysymdef.h
+++ keysymdef.h
@@ -396,6 +396,12 @@ SOFTWARE.
 #define XK_dead_psili                    0xfe64  /* alias for dead_abovecomma */
 #define XK_dead_abovereversedcomma       0xfe65
 #define XK_dead_dasia                    0xfe66  /* alias for dead_abovereversedcomma */
+#define XK_dead_belowring                0xfe67
+#define XK_dead_belowmacron              0xfe68
+#define XK_dead_belowcircumflex          0xfe69
+#define XK_dead_belowtilde               0xfe6a
+#define XK_dead_belowbreve               0xfe6b
+#define XK_dead_belowdiaeresis           0xfe6c
 
 #define XK_First_Virtual_Screen          0xfed0
 #define XK_Prev_Virtual_Screen           0xfed1
