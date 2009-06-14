tmzt/androix-proto-x11proto

commit ced38e880b54f2aae31a5354aecb4235a129f0a4

Remove all trace of X_NOT_STDC_ENV (#6527)
Seriously C89 already you guys.

Xos.h
@@ -85,8 +85,6 @@ in this Software without prior written authorization from The Open Group.
  * which can be really inconvenient. :-(
  */
 
-#ifndef X_NOT_STDC_ENV
-
 #include <string.h>
 #if defined(__SCO__) || defined(__UNIXWARE__)
 #include <strings.h>
@@ -126,12 +124,10 @@ in this Software without prior written authorization from The Open Group.
 #define strrchr rindex
 #endif
 
-#endif /* X_NOT_STDC_ENV */
-
 /*
  * strerror()
  */
-#if (defined(X_NOT_STDC_ENV) || (defined(sun) && !defined(SVR4))) && !defined(__GLIBC__)
+#if (defined(sun) && !defined(SVR4)) && !defined(__GLIBC__)
 #ifndef strerror
 extern char *sys_errlist[];
 extern int sys_nerr;

Xalloca.h
@@ -110,11 +110,7 @@ from The Open Group.
 /* alloca might be a macro taking one arg (hi, Sun!), so give it one. */
 #        if !defined(__sgi) && !defined(__QNX__) && !defined(__cplusplus)
 #          define __Xnullarg		/* as nothing */
-#          ifndef X_NOT_STDC_ENV
-             extern void *alloca(__Xnullarg);
-#          else
-             extern char *alloca(__Xnullarg);
-#          endif
+           extern void *alloca(__Xnullarg);
 #        endif /* !__sgi && !__QNX && !__cplusplus */
 #        define ALLOCATE_LOCAL(size) alloca((int)(size))
 #      endif /* who does alloca */

Xfuncproto.h.in
@@ -1,4 +1,3 @@
-/* $Xorg: Xfuncproto.h,v 1.4 2001/02/09 02:03:22 xorgcvs Exp $ */
 /*
  *
 Copyright 1989, 1991, 1998  The Open Group
@@ -24,7 +23,6 @@ used in advertising or otherwise to promote the sale, use or other dealings
 in this Software without prior written authorization from The Open Group.
  *
  */
-/* $XFree86: xc/include/Xfuncproto.h,v 3.4 2001/12/14 19:53:25 dawes Exp $ */
 
 /* Definitions to make function prototypes manageable */
 

Xfuncs.h
@@ -58,14 +58,14 @@ void bcopy();
 
 /* the new Xfuncs.h */
 
-#if !defined(X_NOT_STDC_ENV) && (!defined(sun) || defined(SVR4))
+#if (!defined(sun) || defined(SVR4))
 /* the ANSI C way */
 #ifndef _XFUNCS_H_INCLUDED_STRING_H
 #include <string.h>
 #endif
 #undef bzero
 #define bzero(b,len) memset(b,0,len)
-#else /* else X_NOT_STDC_ENV or SunOS 4 */
+#else
 #if defined(SYSV) || defined(luna) || defined(sun) || defined(__sxg__)
 #include <memory.h>
 #define memmove(dst,src,len) bcopy((char *)(src),(char *)(dst),(int)(len))
@@ -79,9 +79,9 @@ void bcopy();
 #define memcpy(dst,src,len) bcopy((char *)(src),(char *)(dst),(int)(len))
 #define memcmp(b1,b2,len) bcmp((char *)(b1),(char *)(b2),(int)(len))
 #endif /* SYSV else */
-#endif /* ! X_NOT_STDC_ENV else */
+#endif 
 
-#if defined(X_NOT_STDC_ENV) || (defined(sun) && !defined(SVR4))
+#if (defined(sun) && !defined(SVR4))
 #define atexit(f) on_exit(f, 0)
 #endif
 

Xosdefs.h
@@ -28,51 +28,14 @@ in this Software without prior written authorization from The Open Group.
 #define _XOSDEFS_H_
 
 /*
- * X_NOT_STDC_ENV means does not have ANSI C header files.  Lack of this
- * symbol does NOT mean that the system has stdarg.h.
- *
  * X_NOT_POSIX means does not have POSIX header files.  Lack of this
  * symbol does NOT mean that the POSIX environment is the default.
  * You may still have to define _POSIX_SOURCE to get it.
  */
 
-#ifdef NOSTDHDRS
-#define X_NOT_POSIX
-#define X_NOT_STDC_ENV
-#endif
-
 #ifdef sony
 #if !defined(SYSTYPE_SYSV) && !defined(_SYSTYPE_SYSV)
 #define X_NOT_POSIX
-#endif
-#endif
-
-#ifdef UTEK
-#define X_NOT_POSIX
-#define X_NOT_STDC_ENV
-#endif
-
-#ifdef vax
-#ifndef ultrix			/* assume vanilla BSD */
-#define X_NOT_POSIX
-#define X_NOT_STDC_ENV
-#endif
-#endif
-
-#ifdef luna
-#define X_NOT_POSIX
-#define X_NOT_STDC_ENV
-#endif
-
-#ifdef Mips
-#define X_NOT_POSIX
-#define X_NOT_STDC_ENV
-#endif
-  
-#ifdef USL
-#ifdef SYSV /* (release 3.2) */
-#define X_NOT_POSIX
-#define X_NOT_STDC_ENV
 #endif
 #endif
 
@@ -89,14 +52,7 @@ in this Software without prior written authorization from The Open Group.
 #if !defined(_POSIX_SOURCE)
 #define X_NOT_POSIX
 #endif
-#define X_NOT_STDC_ENV
 #endif
-#endif
-#endif
-
-#ifdef MOTOROLA
-#ifdef SYSV
-#define X_NOT_STDC_ENV
 #endif
 #endif
 
@@ -117,11 +73,6 @@ in this Software without prior written authorization from The Open Group.
 #ifndef _POSIX_
 #define X_NOT_POSIX
 #endif
-#endif
-
-#if defined(nec_ews_svr2) || defined(SX) || defined(PC_UX)
-#define X_NOT_POSIX
-#define X_NOT_STDC_ENV
 #endif
 
 #ifdef __UNIXOS2__

Xpoll.h.in
@@ -1,5 +1,3 @@
-/* $Xorg: Xpoll.h,v 1.4 2001/02/09 02:03:23 xorgcvs Exp $ */
-
 /*
 
 Copyright 1994, 1998  The Open Group
@@ -48,8 +46,6 @@ from The Open Group.
  * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
-
-/* $XFree86: xc/include/Xpoll.h,v 3.8 2001/01/17 17:53:11 dawes Exp $ */
 
 #ifndef _XPOLL_H_
 #define _XPOLL_H_

commit 1f59b5f8eac522bb0a5155393224496c39deca42

Removing dangling #else block from X_NOT_STDC_ENV removal
Fixes mass damage caused by ced38e880b54f2aae31a5354aecb4235a129f0a4
moving the includes for unistd.h, fcntl.h, etc. from the else for
X_NOT_STDC_ENV into the #else for #ifndef _XOS_H_, which made them
go away for everyone.

Signed-off-by: Alan Coopersmith <alan.coopersmith@sun.com>

Xos.h
@@ -81,18 +81,6 @@ in this Software without prior written authorization from The Open Group.
 #endif
 #endif
 
-#else
-
-#ifdef SYSV
-#include <string.h>
-#define index strchr
-#define rindex strrchr
-#else
-#include <strings.h>
-#define strchr index
-#define strrchr rindex
-#endif
-
 /*
  * strerror()
  */